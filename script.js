13eb4c5cffcc2bd01ae347e17354026a8cc96d91
hktari <bkamnik1995@gmail.com>
2022-08-31 13:02:46 +0200

    add product text as overlay effect

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ for (const product of products) {
     product.addEventListener('mouseover', () => {
         console.log('mouse over')
         product.querySelector('.product-title').classList.add('active')
+        product.querySelector('.product-text').classList.remove('w3-hide')
+        product.querySelector('img').classList.add('inactive')
     })
     product.addEventListener('mouseout', () => {
         console.log('mouse out')
         product.querySelector('.product-title').classList.remove('active')
+        product.querySelector('.product-text').classList.add('w3-hide')
+        product.querySelector('img').classList.remove('inactive')
     })
 }
