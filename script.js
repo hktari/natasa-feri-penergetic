a96cacae21d5c4ac327ebe5b0e104697a57badaf
hktari <bkamnik1995@gmail.com>
2022-09-01 11:13:24 +0200

    update product layout and animations

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ const products = document.querySelectorAll('.product-container')
 for (const product of products) {
     product.addEventListener('mouseover', () => {
         console.log('mouse over')
-        product.querySelector('.product-title').classList.add('active')
-        product.querySelector('.product-text').classList.remove('w3-hide')
-        product.querySelector('img').classList.add('inactive')
+        product.classList.add('active')
     })
     product.addEventListener('mouseout', () => {
         console.log('mouse out')
-        product.querySelector('.product-title').classList.remove('active')
-        product.querySelector('.product-text').classList.add('w3-hide')
-        product.querySelector('img').classList.remove('inactive')
+        product.classList.remove('active')
     })
 }
